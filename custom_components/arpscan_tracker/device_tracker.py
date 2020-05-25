ac6f4fe84f6fe15b364f7cf565138da8debcdeb9
Ron Klinkien <ron@cyberjunky.nl>
2020-05-25 21:46:20 +0200

    Added IP address to attributes

--- custom_components/arpscan_tracker/device_tracker.py
+++ custom_components/arpscan_tracker/device_tracker.py
@@ -72,6 +72,14 @@ class ArpScanDeviceScanner(DeviceScanner):
         return mac.replace(':', '')
 
 
+    def get_extra_attributes(self, device):
+        """Return the IP of the given device."""
+        filter_ip = next(
+            (result.ip for result in self.last_results if result.mac == device), None
+        )
+        return {"ip": filter_ip}
+    
+    
     @Throttle(MIN_TIME_BETWEEN_SCANS)
     def _update_info(self):
         """
